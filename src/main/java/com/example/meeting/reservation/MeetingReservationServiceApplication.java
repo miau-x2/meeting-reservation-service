106784994c1501fd83a35b7ac87462e71963d98d
miau-x2 <joined260130@gmail.com>
2026-03-26 20:15:33 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/example/meeting/reservation/MeetingReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.meeting.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingReservationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meeting/reservation/MeetingReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meeting.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
